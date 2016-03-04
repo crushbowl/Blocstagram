c5ff9aa8cc06015e4b99b5c6497ddfbdad884a33
JanL <janelle.h.writes@gmail.com>
2016-03-04 18:25:36 -0500

    Removed Main Storyboard

---
+++ Blocstagram/Blocstagram/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Blocstagram
+//
+//  Created by joy on 3/4/16.
+//  Copyright © 2016 JanL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Blocstagram/Blocstagram/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Blocstagram
+//
+//  Created by joy on 3/4/16.
+//  Copyright © 2016 JanL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
